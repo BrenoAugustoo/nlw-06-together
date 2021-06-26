952832f6342f534e27c954138bc49e9318ad9fdd
BrenoAugustoo <brenoaugusto98@gmail.com>
2021-06-26 18:06:24 -0300

    Aula 05

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './styles/global.scss';
-
 import './services/firebase';
 
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
