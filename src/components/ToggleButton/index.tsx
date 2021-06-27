5af9843a854347df7681e6a5ee31a23d612937b3
BrenoAugustoo <brenoaugusto98@gmail.com>
2021-06-26 21:55:16 -0300

    Próximo nível

---
+++ src/components/ToggleButton/index.tsx
@@ -0,0 +1,26 @@
+import {FormEvent} from 'react'
+import { useTheme } from "../../hooks/useTheme"
+
+import "./styles.scss"
+
+export function ToggleButton() {
+    const { themeName, changeTheme } = useTheme()
+    const changeThemeName = (event: FormEvent) => {
+        event.preventDefault()
+        changeTheme(themeName === 'dark'? "light": "dark")
+    }
+
+    return (
+        <span className="toggleContainer">
+            <button
+                onClick={(event) => changeThemeName(event)}
+                className={`${themeName === "light" ? "light" : "dark"}`}
+                title={`${themeName === 'light'? "Dark": "Light"} mode`}
+            >
+            <span>
+
+            </span>
+            </button>
+        </span>
+    )
+}
